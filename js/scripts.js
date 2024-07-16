f53d9d0d053444b4529e07439fe816921b0ee0ad
maxsalsa <30815006+maxsalsa@users.noreply.github.com>
2024-07-15 22:54:38 -0600

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -10,26 +10,34 @@ function showResults(unit) {
     switch (unit) {
         case 'Fundamentos de Tecnologías de la Información':
             resultText = `
-                <table>
-                    <tr><th>Resultados de Aprendizaje</th></tr>
-                    <tr><td>Emplear componentes técnicos adecuados para la construcción, reparación o actualización de computadoras.</td></tr>
-                    <tr><td>Instalar y configurar componentes para actualizar computadoras según necesidades del usuario.</td></tr>
-                    <tr><td>Determinar procedimientos de comunicación de computadoras en redes.</td></tr>
-                    <tr><td>Explicar soluciones para problemas en equipos portátiles y otros dispositivos.</td></tr>
-                    <tr><td>Instalar sistemas operativos licenciados y de código abierto.</td></tr>
-                    <tr><td>Implementar mecanismos de seguridad para equipos, datos y redes.</td></tr>
+                <table class="table table-striped">
+                    <thead>
+                        <tr><th>Resultados de Aprendizaje</th></tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>Emplear componentes técnicos adecuados para la construcción, reparación o actualización de computadoras.</td></tr>
+                        <tr><td>Instalar y configurar componentes para actualizar computadoras según necesidades del usuario.</td></tr>
+                        <tr><td>Determinar procedimientos de comunicación de computadoras en redes.</td></tr>
+                        <tr><td>Explicar soluciones para problemas en equipos portátiles y otros dispositivos.</td></tr>
+                        <tr><td>Instalar sistemas operativos licenciados y de código abierto.</td></tr>
+                        <tr><td>Implementar mecanismos de seguridad para equipos, datos y redes.</td></tr>
+                    </tbody>
                 </table>
             `;
             break;
         case 'Seguridad Industrial':
             resultText = `
-                <table>
-                    <tr><th>Resultados de Aprendizaje</th></tr>
-                    <tr><td>Mencionar el impacto de las regulaciones nacionales aplicadas en el campo de la Seguridad Industrial.</td></tr>
-                    <tr><td>Explicar los procesos mediante los cuales se realiza el aseguramiento de infraestructuras física.</td></tr>
-                    <tr><td>Aplicar las estrategias de prevención contra riesgos considerando la normativa, protocolos, insumos, equipos y herramientas que se utilizan en las actividades instrumentales.</td></tr>
-                    <tr><td>Utilizar formas creativas e innovadoras para la resolución de problemas cotidianos.</td></tr>
-                    <tr><td>Aplicar medidas preventivas que mitiguen la contaminación de los recursos marinos y sus océanos, promoviendo el desarrollo sostenible en ecosistemas terrestres.</td></tr>
+                <table class="table table-striped">
+                    <thead>
+                        <tr><th>Resultados de Aprendizaje</th></tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>Mencionar el impacto de las regulaciones nacionales aplicadas en el campo de la Seguridad Industrial.</td></tr>
+                        <tr><td>Explicar los procesos mediante los cuales se realiza el aseguramiento de infraestructuras física.</td></tr>
+                        <tr><td>Aplicar las estrategias de prevención contra riesgos considerando la normativa, protocolos, insumos, equipos y herramientas que se utilizan en las actividades instrumentales.</td></tr>
+                        <tr><td>Utilizar formas creativas e innovadoras para la resolución de problemas cotidianos.</td></tr>
+                        <tr><td>Aplicar medidas preventivas que mitiguen la contaminación de los recursos marinos y sus océanos, promoviendo el desarrollo sostenible en ecosistemas terrestres.</td></tr>
+                    </tbody>
                 </table>
             `;
             break;
@@ -38,7 +46,7 @@ function showResults(unit) {
             resultText = '<p>No hay resultados de aprendizaje disponibles.</p>';
     }
 
-    results.innerHTML = resultText + `<button class="mdl-button mdl-js-button mdl-button--raised download-button" onclick="simulatePDFDownload('${unit}')">Descargar en PDF</button>`;
+    results.innerHTML = resultText + `<button class="btn btn-danger mt-3" onclick="simulatePDFDownload('${unit}')">Descargar en PDF</button>`;
 }
 
 function simulatePDFDownload(unitName) {
