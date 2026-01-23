6a88f416e8e283c822fae2a3a8fd4b8f602e3597
Max <maxi.salsa@gmailcom>
2026-01-23 15:44:36 -0600

    UPDATE: Configuración final para despliegue exitoso

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! ADVERTENCIA !!
-    // Ignoramos errores para forzar el despliegue en producción
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Ignoramos errores de estilo
     ignoreDuringBuilds: true,
   },
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-﻿import React from 'react' // <--- ESTO FALTABA
+﻿import React from 'react'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
